b426ac7409ce9cfe4077e74f09485dcbd2708e0e
RossClarkScotland <rossclarkscotland@gmail.com>
2022-05-01 19:34:49 +0000

    configure allauth and create account templates

--- gaelic_galore/settings.py
+++ gaelic_galore/settings.py
@@ -37,10 +37,32 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
-    #local
+    # 3rd party
+    'allauth',
+    'allauth.account',
+
+    # local
     'pages',
 ]
+
+# allauth configuration
+# I learned this set-up method from 'Django for Professionals'
+SITE_ID = 1
+LOGIN_REDIRECT_URL = 'home'
+ACCOUNT_LOGOUT_REDIRECT_URL = 'home'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+DEFAULT_FROM_EMAIL = 'rossclarkscotland@gmail.com'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
