ae7ff4b4afb794a848def35f530feff85b34266e
RossClarkScotland <rossclarkscotland@gmail.com>
2022-05-02 19:53:09 +0000

    add Courses app admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Location, Course
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("name", "description_place", "description_accomodation")
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "level", "date", "price")
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(Course)
