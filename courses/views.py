490ecd9714c1e223f2f78c014cb14d5eb4e321dc
RossClarkScotland <rossclarkscotland@gmail.com>
2022-05-04 08:52:59 +0000

    add navbar dropdowns for Courses+Locations

--- courses/views.py
+++ courses/views.py
@@ -3,11 +3,13 @@ from .models import Course
 
 # Create your views here.
 class CourseListView(ListView):
+# returns all courses
     model = Course
     context_object_name = 'course_list'
     template_name = 'courses/course_list.html'
 
 class CourseDetailView(DetailView):
+# returns individual courses
     model = Course
     context_object_name = 'course'
     template_name = 'courses/course_detail.html'
--- locations/views.py
+++ locations/views.py
@@ -3,11 +3,13 @@ from .models import Location
 
 # Create your views here.
 class LocationListView(ListView):
+# returns all locations
     model = Location
     context_object_name = 'location_list'
     template_name = 'locations/location_list.html'
 
 class LocationDetailView(DetailView):
+# returns individual locations
     model = Location
     context_object_name = 'location'
     template_name = 'locations/location_detail.html'
