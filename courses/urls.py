eda4d2b5f9da32c1a2de0480dbef08b532f1036c
RossClarkScotland <rossclarkscotland@gmail.com>
2022-05-04 18:08:13 +0000

    wire up navbar search functionality

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CourseListView, CourseDetailView
+from .views import CourseListView, CourseDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', CourseListView.as_view(), name='course_list'),
     path('<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from .models import Course
+from django.db.models import Q
 
 # Create your views here.
 class CourseListView(ListView):
@@ -14,3 +15,19 @@ class CourseDetailView(DetailView):
     context_object_name = 'course'
     template_name = 'courses/course_detail.html'
 
+class SearchResultsListView(ListView):
+    model = Course
+    context_object_name = 'course_list'
+    template_name = 'courses/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Course.objects.filter(
+            Q(location__name__icontains=query) |
+            Q(location__description_place__icontains=query) |
+            Q(location__description_accomodation__icontains=query) |
+            Q(title__icontains=query)    |
+            Q(level__icontains=query)
+        )
+    """ Note from the above the different syntax required when
+    searching on a foreign key!!!!"""
