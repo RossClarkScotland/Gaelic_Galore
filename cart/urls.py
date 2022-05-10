d5190914966b9e8fd66d9ac6cda67220d881073b
RossClarkScotland <rossclarkscotland@gmail.com>
2022-05-10 20:31:49 +0000

    wire up product update+remove btns in cart

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', CartView.as_view(), name='cart'),
-    path('add/<course_id>', views.add_cart_item, name='add_cart_item'),
+    path('add/<item_id>', views.add_cart_item, name='add_cart_item'),
+    path('adjust_cart/<item_id>', views.adjust_cart_item_qty, name='adjust_cart'),
+    path('remove_cart_item/<item_id>', views.remove_cart_item, name='remove_cart_item'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.views.generic import TemplateView
 
 # Create your views here.
@@ -8,17 +8,47 @@ class CartView(TemplateView):
 
 
 
-def add_cart_item(request, course_id):
+def add_cart_item(request, item_id):
 # adds item to cart
 # logic pattern learned from Boutique Ado walktrhrough project
     qty = int(request.POST.get('qty'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if course_id in list(cart.keys()):
-        cart[course_id] += qty
+    if item_id in list(cart.keys()):
+        cart[item_id] += qty
     else:
-        cart[course_id] = qty
+        cart[item_id] = qty
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+ 
+
+def adjust_cart_item_qty(request, item_id):
+# changes number of items in the cart
+# logic pattern learned from Boutique Ado walktrhrough project
+    qty = int(request.POST.get('qty'))
+    cart = request.session.get('cart', {})
+
+    if qty > 0:
+        cart[item_id] = qty
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
+
+
+def remove_cart_item(request, item_id):
+# removes items from the cart
+# logic pattern learned from Boutique Ado walktrhrough project
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
