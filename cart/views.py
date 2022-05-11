e7d678c20774941dd5baf7eec063f220f7ca2f45
RossClarkScotland <rossclarkscotland@gmail.com>
2022-05-11 19:55:23 +0000

    wire up cart preview in toast_success.html

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.views.generic import TemplateView
 from django.contrib import messages
 from courses.models import Course
@@ -13,13 +13,14 @@ class CartView(TemplateView):
 def add_cart_item(request, item_id):
 # adds item to cart
 # logic pattern learned from Boutique Ado walktrhrough project
-    course = Course.objects.get(pk=item_id)
+    course = get_object_or_404(Course, pk=item_id)
     qty = int(request.POST.get('qty'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += qty
+        messages.success(request, f'Number of {course.title} participants updated!')
     else:
         cart[item_id] = qty
         messages.success(request, f'{course.title} added to cart')
@@ -32,13 +33,16 @@ def add_cart_item(request, item_id):
 def adjust_cart_item_qty(request, item_id):
 # changes number of items in the cart
 # logic pattern learned from Boutique Ado walktrhrough project
+    course = get_object_or_404(Course, pk=item_id)
     qty = int(request.POST.get('qty'))
     cart = request.session.get('cart', {})
 
     if qty > 0:
         cart[item_id] = qty
+        messages.success(request, f'Number of {course.title} participants updated!')
     else:
         cart.pop(item_id)
+        messages.success(request, f'{course.title} removed from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('cart'))
@@ -47,7 +51,9 @@ def adjust_cart_item_qty(request, item_id):
 def remove_cart_item(request, item_id):
 # removes items from the cart
 # logic pattern learned from Boutique Ado walktrhrough project
+
     try:
+        course = get_object_or_404(Course, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
 
