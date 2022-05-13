702ee43dd867b63116eb22c7c2fb3240e750135b
RossClarkScotland <rossclarkscotland@gmail.com>
2022-05-13 19:54:32 +0000

    set up checkout functionality in stripe.js

--- gaelic_galore/settings.py
+++ gaelic_galore/settings.py
@@ -161,6 +161,11 @@ STATICFILES_FINDERS = [
 
 ]
 
+# Stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
