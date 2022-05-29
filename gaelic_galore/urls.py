d3a69485d2431a361efad6b908e3a2c062ef0749
RossClarkScotland <rossclarkscotland@gmail.com>
2022-05-29 19:30:08 +0000

    add favicon

--- gaelic_galore/urls.py
+++ gaelic_galore/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
@@ -10,6 +12,10 @@ urlpatterns = [
 
     # allauth
     path('accounts/', include('allauth.urls')),
+
+    # favicon
+	 path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'))),
+
 
     # my local apps
     path('', include('pages.urls')),
