23664290dcb6bff2f53eaaafe743b743945e93d6
RossClarkScotland <rossclarkscotland@gmail.com>
2022-06-05 18:38:34 +0000

    update purchase confirmation email

--- gaelic_galore/settings.py
+++ gaelic_galore/settings.py
@@ -17,7 +17,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True
 
 ALLOWED_HOSTS = ['gaelic-galore.herokuapp.com', 'localhost']
 
